80a3d44ae82a738b276b0453fe133ed39e087d11
Mahir Emre Çetin <emrecetinxx@gmail.com>
2026-04-24 19:00:53 +0300

    fix: correct ObjectId type definition in Order model

--- models/order.js
+++ models/order.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema(
   {
     user: {
-      type: mongoose.Schema.types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
       required: [true, 'Kullanici bilgisi zorunludur'],
     },
