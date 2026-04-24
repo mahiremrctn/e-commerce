cc17c8e9eaa62f401b0d719e1368da2acb364f97
Mahir Emre Çetin <emrecetinxx@gmail.com>
2026-04-24 03:18:38 +0300

    feat: define checkout form request object with buyer and basket details

--- controllers/iyzicoPaymentController.js
+++ controllers/iyzicoPaymentController.js
@@ -64,3 +64,48 @@ const createCheckoutForm = async (req, res) => {
             paymentStatus: 'pending',
             paymentProvider: 'iyzico',
         });
+
+        const checkoutFormRequest = {
+            locale: Iyzipay.LOCALE.TR,
+            conversationId: order._id.toString(),
+            price: totalPrice,
+            paidPrice: totalPrice,
+            currency: Iyzipay.CURRENCY.TRY,
+            basketId: order._id.toString(),
+            paymentGroup: Iyzipay.PAYMENT_GROUP.PRODUCT,
+            callbackUrl: `${getBaseUrl()}/api/iyzico-payments/callback`,
+            enabledInstallments: [1, 2, 3, 6, 9],
+            buyer: {
+                id: userId,
+                name: 'Test',
+                surname: 'Kullanici',
+                gsmNumber: '+905350000000',
+                email: 'test@test.com',
+                identityNumber: '11111111111',
+                registrationAddress: 'Test Mahallesi Test Sokak No:1',
+                ip: req.ip || '85.34.78.112',
+                city: 'Istanbul',
+                country: 'Turkey',
+            },
+            shippingAddress: {
+                contactName: 'Test Kullanici',
+                city: 'Istanbul',
+                country: 'Turkey',
+                address: 'Test Mahallesi Test Sokak No:1',
+            },
+            billingAddress: {
+                contactName: 'Test Kullanici',
+                city: 'Istanbul',
+                country: 'Turkey',
+                address: 'Test Mahallesi Test Sokak No:1',
+            },
+            basketItems: [
+                {
+                    id: product._id.toString(),
+                    name: product.name,
+                    category1: 'Genel',
+                    itemType: Iyzipay.BASKET_ITEM_TYPE.PHYSICAL,
+                    price: totalPrice,
+                },
+            ],
+        };
