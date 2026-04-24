aee5c90927aeba2a1d75a6ab163efca1396614d7
Mahir Emre Çetin <emrecetinxx@gmail.com>
2026-04-24 03:05:09 +0300

    feat: add payment path helper and checkout result promise

--- controllers/iyzicoPaymentController.js
+++ controllers/iyzicoPaymentController.js
@@ -5,6 +5,26 @@ const Product = require('../models/Product');
 const Order = require('../models/Order');
 const { log } = require('console');
 
-getBaseUrl = () => {
+const getBaseUrl = () => {
   return (process.env.BASE_URL || 'http://localhost:3000').replace(/\/$/, '');
+};
+
+const getPaymentPagePath = (pageName) => {
+    return path.join(__dirname, '..', 'views', pageName);
+};
+
+const retrieveCheckoutFormResult = (checkoutFormRequest) => {
+    return new Promise((resolve, reject) => {
+        iyzipay.checkoutForm.retrieve(
+            checkoutFormRequest,
+            (error, result) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+
+                resolve(result);
+            },
+        );
+    });
 };
